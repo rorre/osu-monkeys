c65abbb53ae3085c4783ef70326331c3f5959336
Rendy Arya Kemal <rendyarya22@gmail.com>
2020-09-04 10:23:34 +0700

    Hide on changing tabs or changing diff

--- osuhidehype.user.js
+++ osuhidehype.user.js
@@ -9,23 +9,24 @@
 // @run-at       document-idle
 // ==/UserScript==
 
+// TODO: Save hide state? seems like gonna be an unnecessary work to do but maybe, just maybe /shrug
 function initApplication() {
     // lol nice meme
     // lazy to make complex shit so lemme just copy paste my html xd
     var checkbox_html = $('<label class="praisehide beatmap-discussions__toolbar-item beatmap-discussions__toolbar-item--link beatmap-discussion-new__notice-checkbox"><div class="osu-switch-v2"><input id="isPraiseHide" class="osu-switch-v2__input" type="checkbox"><span class="osu-switch-v2__content"></span></div>Hide all hype/praise</label>')
     var toolbar = $(".beatmap-discussions__toolbar-content--right")
 
-    // Busy wait until initial osu!js for rendering is done
+    // Busy wait until initial osu!js for rendering is done, else everything else will simply break
     while (toolbar.length == 0) {
         toolbar = $(".beatmap-discussions__toolbar-content--right")
     }
 
+    var praises;
     toolbar.prepend(checkbox_html)
     checkbox_html.click(function () {
-        // TBH don't want to check every praises here but like sometimes if we store everything outside
-        // it simply just doesnt work after changing tab, idk why.
-        // so yeah lets just find everything
-        var praises = $(".beatmap-discussion-message-type--hype, .beatmap-discussion-message-type--praise").parents(".beatmap-discussions__discussion")
+        // Praises seems to be different for each tab so let's just restore the praises of current tab
+        // into the variable, so it can also be used for reshowing when changing tabs.
+        praises = $(".beatmap-discussion-message-type--hype, .beatmap-discussion-message-type--praise").parents(".beatmap-discussions__discussion")
         var e = $("#isPraiseHide")[0].checked
 
         // The performance of removing the elements is much better but since its a checkbox,
@@ -36,6 +37,12 @@ function initApplication() {
         } else {
             praises.show()
         }
+    })
+
+    $(".page-mode__item, beatmap-list__item").click(function () {
+        // Show the praises again once changing tab OR changing difficulty.
+        if (praises) praises.show()
+        $("#isPraiseHide")[0].checked = false
     })
 }
 
